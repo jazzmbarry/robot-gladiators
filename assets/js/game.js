03233cac0dbd80cf0c3b8fdd8118a7bad950ac2e
jazzmbarry <jazzmbarry@gmail.com>
2021-08-01 15:41:30 -0600

    randomhealth and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,14 +30,16 @@ var fight = function(enemyName) {
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
       }
   
       // remove enemy's health by subtracting the amount set in the playerAttack variable
-      enemyHealth = enemyHealth - playerAttack;
+      var damage = randomNumber(playerAttack - 3, playerAttack);
+      
+      enemyHealth = Math.max(0, enemyHealth - damage);
       console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
       );
@@ -56,7 +58,8 @@ var fight = function(enemyName) {
       }
   
       // remove players's health by subtracting the amount set in the enemyAttack variable
-      playerHealth = playerHealth - enemyAttack;
+      var damage = randomNumber(enemyAttack - 3, enemyAttack)
+      playerHealth = Math.max(0, playerHealth - damage);
       console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
       );
@@ -86,7 +89,7 @@ var startGame = function(){
             var pickedEnemyName = enemyNames[i];
         
             // reset enemyHealth before starting new fight
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
         
             // use debugger to pause script from running and check what's going on at that moment in the code
             // debugger;
@@ -188,6 +191,12 @@ var shop = function() {
             shop();
             break;
     }
+};
+
+var randomNumber = function() {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
 };
 // run fight function to start game
 // fight();
