57e58df63fe2233639d510824d470abe664c3d00
jazzmbarry <jazzmbarry@gmail.com>
2021-07-31 19:47:11 -0600

    fixed robot right loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,16 +11,11 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-// for(var i = 0; i < enemyNames.length; i++) {
-//     console.log(enemyNames[i]);
-//     console.log(i);
-//     console.log(enemyNames[i] + " is at " + i + " index");
-//   }
-
 // fight function
 var fight = function(enemyName) {
+    while(enemyHealth>0) {
   // Alert players that they are starting the round
-  window.alert("Welcome to Robot Gladiators!");
+//   window.alert("Welcome to Robot Gladiators!");
 
   // ask player if they'd like to fight or run
   var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
@@ -71,11 +66,14 @@ var fight = function(enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+  }
 };
 
-// run fight function to start game
-// fight();
-
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-}
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50
+    fight(pickedEnemyName)
+  }
+// run fight function to start game
+fight();
+
